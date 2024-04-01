4f9b48507bcd1780837cd803e048adf1c8677632
vandana002 <vandanam448@gmail.com>
2024-04-01 18:34:10 +0530

    Add new files

--- src/test/java/Runner/Hotel.java
+++ src/test/java/Runner/Hotel.java
@@ -1,16 +1,18 @@
 package Runner;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features="Features",monochrome=true,glue="StepDefinitions",
 //plugin="com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 plugin={"pretty","json:target/cucumberReports.json"})
 
 //plugin={"pretty","junit:target/cucumberReports.xml"})
-public class Hotel {
+public class Hotel extends AbstractTestNGCucumberTests{
 	
 }
